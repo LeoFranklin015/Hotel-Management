1c00a0a7a39be53154efa78c04fd3d192f4b744c
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2022-12-11 16:02:42 +0530

    window problem cleared

--- src/application/App.java
+++ src/application/App.java
@@ -1,28 +1,33 @@
 package application;   
 import javafx.application.Application;  
-import javafx.event.ActionEvent;  
-import javafx.event.EventHandler;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;  
-import javafx.scene.control.Button;  
+
 import javafx.stage.Stage;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;  
+
 public class App extends Application{  
   
     @Override  
     public void start(Stage primaryStage)  {  
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("frame.fxml"));
+            try{
             Parent root = fxmlLoader.load();
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
+            }
+            catch(NullPointerException e1)
+            {
+                System.out.println("ERROR");
+            }
+            
             primaryStage.setTitle("Hotel Management");
             primaryStage.show();
 
         } catch (Exception e) {
-            
+            e.printStackTrace();
         }
 
          
